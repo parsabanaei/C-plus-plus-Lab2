ca2dd2277fe93bee136ca8217cce27d86482c968
parsa banaei <parsa.banaei21@gmail.com>
2022-08-25 22:13:29 -0700

    Add project files.

---
+++ ConsoleApplicationLab2/inherit2i.cpp
@@ -0,0 +1,38 @@
+#include <iostream> 
+using namespace std;
+#include <string> 
+#include "inherit2.h" 
+
+using namespace std;
+
+
+void personType::print() const
+{
+	cout << firstName << " " << lastName;
+}
+
+void personType::setName(string first, string last)
+{
+	firstName = first;
+	lastName = last;
+}
+
+void personType::getName(string& first, string& last)
+{
+	first = firstName;
+	last = lastName;
+}
+
+//constructor with parameters 
+personType::personType(string first, string last)
+
+{
+	firstName = first;
+	lastName = last;
+}
+
+personType::personType()   //default constructor 
+{
+	firstName = "";
+	lastName = "";
+}
---
+++ ConsoleApplicationLab2/main.cpp
@@ -0,0 +1,36 @@
+// Author: Parsa Banaei and Adrian
+// Description: Hello World in C++ 
+//============================================================ 
+
+#include <iostream> 
+
+#include "partTimeEmployee.h"
+//#include "inherit2.h" 
+
+//#include "hello1Function.cpp"
+//#include "hello2Function.cpp"
+using namespace std;
+
+int hello1Function(string name1);
+int hello2Function(string name2);
+
+int main()
+{
+	string name1 = "Parsa";
+	hello1Function(name1);
+	string name2 = "Adrian";
+	hello2Function(name2);
+
+
+
+
+	personType newPerson();
+	partTimeEmployee newEmployee("John", "Smith", 7.50, 56);
+	partTimeEmployee employee;
+    newEmployee.print();
+	employee.setNameRateHours("Rachel", "Moore",9.75, 45); 
+	employee.print(); 
+
+	return 0;
+}
+
